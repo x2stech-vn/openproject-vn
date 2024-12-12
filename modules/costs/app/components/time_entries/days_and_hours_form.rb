7ac4ec1dc54cdead8fdc950b1f4238fd0e426468
Klaus Zanders <k.zanders@openproject.com>
2024-12-12 18:46:24 +0100

    split form into seperate fields

---
+++ modules/costs/app/components/time_entries/days_and_hours_form.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module TimeEntries
+  class DaysAndHoursForm < ApplicationForm
+    delegate :project, to: :model
+
+    form do |f|
+      f.single_date_picker name: :spent_on,
+                           type: "date",
+                           required: true,
+                           datepicker_options: { inDialog: true },
+                           value: model.spent_on&.iso8601,
+                           label: TimeEntry.human_attribute_name(:spent_on)
+
+      if show_start_and_end_time_fields?
+        f.group(layout: :horizontal) do |g|
+          g.text_field name: :start_time,
+                       type: "time",
+                       required: true,
+                       label: TimeEntry.human_attribute_name(:start_time),
+                       value: model.start_timestamp&.strftime("%H:%M"),
+                       data: {
+                         "time-entry-target" => "startTimeInput",
+                         "action" => "input->time-entry#timeInputChanged"
+                       }
+
+          g.text_field name: :end_time,
+                       type: "time",
+                       required: true,
+                       label: TimeEntry.human_attribute_name(:end_time),
+                       value: model.end_timestamp&.strftime("%H:%M"),
+                       data: {
+                         "time-entry-target" => "endTimeInput",
+                         "action" => "input->time-entry#timeInputChanged"
+                       }
+        end
+      end
+
+      f.text_field name: :hours,
+                   required: true,
+                   label: TimeEntry.human_attribute_name(:hours),
+                   value: hours_value,
+                   data: { "time-entry-target" => "hoursInput",
+                           "action" => "blur->time-entry#hoursChanged keypress.enter->time-entry#hoursKeyEnterPress" }
+    end
+
+    private
+
+    def show_start_and_end_time_fields?
+      TimeEntry.can_track_start_and_end_time?
+    end
+
+    def hours_value
+      if model.hours.present?
+        ChronicDuration.output(model.hours * 3600, format: :hours_only)
+      else
+        ""
+      end
+    end
+  end
+end
---
+++ modules/costs/app/components/time_entries/user_form.rb
@@ -0,0 +1,80 @@
+# frozen_string_literal: true
+
+#-- copyright
+# OpenProject is an open source project management software.
+# Copyright (C) the OpenProject GmbH
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License version 3.
+#
+# OpenProject is a fork of ChiliProject, which is a fork of Redmine. The copyright follows:
+# Copyright (C) 2006-2013 Jean-Philippe Lang
+# Copyright (C) 2010-2013 the ChiliProject Team
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# See COPYRIGHT and LICENSE files for more details.
+#++
+
+module TimeEntries
+  class UserForm < ApplicationForm
+    form do |f|
+      if show_user_field?
+        f.autocompleter(
+          name: :user_id,
+          id: "time_entry_user_id",
+          label: TimeEntry.human_attribute_name(:user),
+          required: true,
+          autocomplete_options: {
+            defaultData: true,
+            hiddenFieldAction: "change->time-entry#userChanged",
+            component: "opce-user-autocompleter",
+            url: ::API::V3::Utilities::PathHelper::ApiV3Path.principals,
+            filters: user_completer_filters,
+            searchKey: "any_name_attribute",
+            resource: "principals",
+            focusDirectly: false,
+            multiple: false,
+            appendTo: "#time-entry-dialog",
+            disabled: !show_user_field?
+          }
+        )
+      end
+    end
+
+    private
+
+    delegate :project, to: :model
+
+    def show_user_field?
+      # Only allow setting a different user, when the user has the
+      # permission to log time for others in the project
+      User.current.allowed_in_project?(:log_time, project)
+    end
+
+    def user_completer_filters
+      filters = [
+        { name: "type", operator: "=", values: %w[User Group] },
+        { name: "status", operator: "=", values: [Principal.statuses[:active], Principal.statuses[:invited]] }
+      ]
+
+      if model.project_id
+        filters << { name: "member", operator: "=", values: [model.project_id] }
+      end
+
+      filters
+    end
+  end
+end
