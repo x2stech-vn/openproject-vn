7627b4f32ee7e961a5e5ecc7f802fa871bef4325
Klaus Zanders <k.zanders@openproject.com>
2024-12-20 11:25:19 +0100

    emit a custom event when the dialog is closed

--- frontend/src/turbo/turbo-event-listeners.ts
+++ frontend/src/turbo/turbo-event-listeners.ts
@@ -6,14 +6,20 @@ export function addTurboEventListeners() {
   //
   // It is necessary to close the primer dialog using the `close()` method, otherwise
   // it will leave an overflow:hidden attribute on the body, which prevents scrolling on the page.
+  //
+  // Also, we will dispatch a custom `dialog:close` event when the dialog is closed.
   document.addEventListener('turbo:submit-end', (event:CustomEvent) => {
-    const { detail: { success }, target } = event as { detail:{ success:boolean }, target:EventTarget };
+    const { detail: { success }, target } = event as { detail:{ success:boolean }; target:EventTarget };
 
     if (success && target instanceof HTMLFormElement) {
       const dialog = target.closest('dialog') as HTMLDialogElement;
 
-      if (dialog && dialog.dataset.keepOpenOnSubmit !== 'true') {
-        dialog.close();
+      if (dialog) {
+        document.dispatchEvent(new CustomEvent('dialog:close', { detail: { dialog } }));
+
+        if (dialog.dataset.keepOpenOnSubmit !== 'true') {
+          dialog.close();
+        }
       }
     }
   });
